d85bfa5a3964fd22aa7ba25faf0006dc56be5023
yuqiufu <1327499522@qq.com>
2019-06-11 16:10:51 +0800

    spring项目练习

---
+++ spring-ideademo/src/test/java/com/sam/springideademo/SpringIdeademoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sam.springideademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringIdeademoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
