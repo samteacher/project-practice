bfd84c56d413abb868a260965e140cb61daad796
yuqiufu <1327499522@qq.com>
2019-06-20 19:47:35 +0800

    提交网关代码

--- springbootall/eureka-zuul/src/main/java/com/sam/zuul/ZuulApplication.java
+++ springbootall/eureka-zuul/src/main/java/com/sam/zuul/ZuulApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
 public class ZuulApplication {
 
     public static void main(String[] args) {
