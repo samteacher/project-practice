af3c264d8fadaa66cdce8b3edd9962eff9aeae2a
yuqiufu <1327499522@qq.com>
2019-07-04 16:42:00 +0800

    分布式配置中心服务

--- springbootall/config-server/src/main/java/com/sam/config/ConfigApplication.java
+++ springbootall/config-server/src/main/java/com/sam/config/ConfigApplication.java
@@ -2,7 +2,16 @@ package com.sam.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 分布式配置中心 -- 服务中心
+ *
+ * @author yuqiufu
+ * @since 2019-07-03
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
