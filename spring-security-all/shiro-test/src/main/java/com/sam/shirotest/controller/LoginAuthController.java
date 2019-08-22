088087dfbdaff14070c7d09b6a2ead4a5c485fa4
yuqiufu <1327499522@qq.com>
2019-08-22 19:25:52 +0800

    测试shiro认证

--- spring-security-all/shiro-test/src/main/java/com/sam/shirotest/controller/LoginAuthController.java
+++ spring-security-all/shiro-test/src/main/java/com/sam/shirotest/controller/LoginAuthController.java
@@ -1,14 +1,12 @@
 package com.sam.shirotest.controller;
 
 import com.sam.shirotest.commen.Response;
+import com.sam.shirotest.entity.SystemUser;
 import com.sam.shirotest.utils.AuthenticationUtil;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  * 登录认证授权测试
@@ -27,18 +25,21 @@ public class LoginAuthController {
     }
 
     @PostMapping("/login")
-    public Response<Map<String, String>> testLogin(Map<String, String> mapResponse) {
-        Response response = new Response();
-        String username = mapResponse.get("username");
-        String password = mapResponse.get("password");
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-        boolean flag = AuthenticationUtil.getToken(token);
-        System.out.println("isAuthenticated" + flag);
-        if (flag) {
-            return Response.success(1, "授权成功");
-        } else {
-            return Response.error(-1, "授权失败");
+    public Response testLogin(SystemUser systemUser) {
+        if (systemUser != null) {
+            System.out.println(systemUser);
+            String username = systemUser.getUsername();
+            String password = systemUser.getPassword();
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            boolean flag = AuthenticationUtil.getToken(token);
+            System.out.println("isAuthenticated" + flag);
+            if (flag) {
+                return Response.data(systemUser,1, "授权成功");
+            } else {
+                return Response.error(-1, "授权失败");
+            }
         }
+        return Response.error(-1, "授权失败");
     }
 
 
